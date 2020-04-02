ecaf0bf75fb56d4b4b0dfda28394aba8c459940f
andrecristiani <aluis02@hotmail.com>
2020-04-02 00:16:01 -0300

    first commit

---
+++ src/main/java/com/spring/codeblog/service/ServiceImpl/PostServiceImpl.java
@@ -0,0 +1,30 @@
+package com.spring.codeblog.service.ServiceImpl;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.PostRepository;
+import com.spring.codeblog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+    @Autowired
+    PostRepository postRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findById(long id) {
+        return postRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+}
